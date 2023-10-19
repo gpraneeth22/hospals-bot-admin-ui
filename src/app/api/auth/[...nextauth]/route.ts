bfd140087ed37bc665b9dfa1e0734f01edc14032
gpraneeth22 <gpraneeth22@gmail.com>
2023-10-19 18:11:26 +0530

    Added Secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,8 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-    ]
+    ],
+    secret: "asdfghjkl456"
 })
 
 export { handler as GET, handler as POST }
